e0bc417069ba95b4a15020ac0f024343cb3ceb7e
= <=>
2024-08-11 14:12:24 -0400

    Iniciamos la imagen jenkins en testhome

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,4 +1,4 @@
 # sudo chown -R 1000:1000 /home/carlos/git/jenkins/home
 docker context use default
 docker rm -f jblueQ
-docker run -d --name=jblueQ -p 8091:8080 -p 50000:50000 -v C:/Users/Pelao/Desktop/git/jenkins/testhome:/var/jenkins_home jenkins-cc:v4 
+docker run -d --name=jblueQ -p 8091:8080 -p 50000:50000 -v C:/Users/Pelao/Desktop/git/jenkins/docker/testhome:/var/jenkins_home jenkins-cc:v4 
